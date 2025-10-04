7da29337aed1e5693b4c36718f4f4a85787f99bd
Takamuracomics <Takamuracomics@gmail.com>
2025-10-04 15:41:11 +0530

    intitial commit for remainig files

--- src/app.js
+++ src/app.js
@@ -11,3 +11,16 @@ app.use(
 );
 
 app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({  extended : true ,limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/user", userRouter);
+
+
+
+
+
+export {app};
